1c997be5a5f6d0c595deed9e4eae4f54a247ab1e
Nikola Popov <nikola.popov01@sap.com>
2022-04-18 18:40:56 +0300

    Initial commit :tada:

---
+++ src/main/java/bg/nbu/electronicgradebook/ElectronicGradebookApplication.java
@@ -0,0 +1,13 @@
+package bg.nbu.electronicgradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicGradebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicGradebookApplication.class, args);
+	}
+
+}
